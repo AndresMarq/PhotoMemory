2a669651dd53bfb2ce5fbd7a026a6d2da363f526
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-08-12 21:05:56 -0600

    Initial Commit

---
+++ PhotoMemory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoMemory
+//
+//  Created by Andres Marquez on 2021-08-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoMemory/PhotoMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMemoryApp.swift
+//  PhotoMemory
+//
+//  Created by Andres Marquez on 2021-08-12.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
